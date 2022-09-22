ae0e16fb24081d5756e89eaae88526c86ed3d850
BranBao1995 <brandon.0702@hotmail.com>
2022-09-22 16:36:28 -0400

    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Work-Day-Scheduler
+
 Bootcamp Module 5 challenge of building a Work Day Scheduler with help from third-party APIs.
+
+## Description
+
+- This project involves third-party APIs such as Bootstrap, Moment.js, and J-Query.
+- This is a single-day task tracker designed to help you keep track of what you currently need to do, what have you done/missed, and what will you need to do in the upcoming hours.
+- Time blocks are highlighted with different colors to signify if it is in the past, present, or future.
+- User can click on any time block to start entering/editing his/her task for that specific hour.
+- A 'save task' is also available for users to save their tasks, so that even if the page refreshes the tasks will not disappear.
+- A 'saved successfully' message will pop up to notify the user their task has been saved.
+
+## Usage
+
+- The current date will be shown in the header:
+  ![Header that shows current date](./assets/images/date-display.JPG);
+
+- Time blocks are highlighted with different colors:
+  ![time blocks with different colors](./assets/images/timeBlocks-highlight.JPG);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-const currentDayDisplay = $("#currentDay");
+const currentDayDisplay = $("#currentDay"); // date display
 const timeBlocksContainer = $(".container-fluid");
 
-const currentDay = moment().format("dddd, MMMM Do, YYYY");
-const currentTimeDisplay = moment().format("hA");
-const currentTime = moment(currentTimeDisplay, "hA");
+const currentDay = moment().format("dddd, MMMM Do, YYYY"); // current date with a specific format
+const currentTimeDisplay = moment().format("hA"); // current time fetched with AM/PM format
+const currentTime = moment(currentTimeDisplay, "hA"); // current time with minutes and seconds omitted, so we can only compare the hours.
 
 const timeSlots = [
   "9AM",
@@ -19,10 +19,11 @@ const timeSlots = [
   "7PM",
 ];
 
-let saveButtonArray = [];
-let inputArray = [];
-let savedSuccessfully = [];
+let saveButtonArray = []; // array to store all 'save' buttons
+let inputArray = []; // array to store all user input fields
+let savedSuccessfully = []; // array to store all "saved successfully" messages
 
+// an object storing only the input tasks
 let savedTasks = {
   task: [],
 };
@@ -30,6 +31,7 @@ let savedTasks = {
 init();
 
 function init() {
+  // fetch from local storage for all saved tasks
   if (localStorage.getItem("savedTasks") != null) {
     savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
   } else {
@@ -38,27 +40,32 @@ function init() {
     };
   }
 
+  // display current date in the header
   currentDayDisplay.text(currentDay);
 
+  // start rendering UI elements
   renderElements();
 }
 
 function renderElements() {
-  const timeBlocksUl = $("<ul>");
-  timeBlocksUl.attr("class", "custom-timeBlocks");
-  timeBlocksContainer.append(timeBlocksUl);
+  const timeBlocksUl = $("<ul>"); // create a list to store all time blocks
+  timeBlocksUl.attr("class", "custom-timeBlocks"); // give it a class to add styling
+  timeBlocksContainer.append(timeBlocksUl); // append newly created <ul>
 
+  // for each work hour
   for (let i = 0; i < timeSlots.length; i++) {
+    // create all necessary UI elements
     let timeBlock = $("<li>");
     let timeLabel = $("<label>");
     let taskInput = $("<input>");
     let saveIcon = $("<button>");
     let saved = $("<span>");
 
-    timeLabel.text(timeSlots[i]);
-    saved.text("Saved Successfully");
-    taskInput.val(savedTasks.task[i]);
+    timeLabel.text(timeSlots[i]); // display the target work hour on the left of the time block
+    saved.text("Saved Successfully"); // set text content to be rendered when a user saves
+    taskInput.val(savedTasks.task[i]); // all input fields will be filled with saved tasks at a specific hour
 
+    // append elements
     timeBlocksUl.append(timeBlock);
     timeBlock
       .append(timeLabel)
@@ -69,11 +76,13 @@ function renderElements() {
 
     taskInput.attr("type", "text");
 
+    // add classes to give styling
     timeBlock.addClass("custom-time-block custom-row");
     timeLabel.addClass("custom-hour");
     saved.addClass("custom-saved");
     saveIcon.addClass("custom-saveBtn");
 
+    // add classes conditionally to the input fields based on the current time
     if (moment(timeSlots[i], "hA").isSame(currentTime)) {
       taskInput.addClass("custom-input custom-present");
     } else if (moment(timeSlots[i], "hA").isBefore(currentTime)) {
@@ -82,6 +91,7 @@ function renderElements() {
       taskInput.addClass("custom-input custom-future");
     }
 
+    // push all created elements into respective arrays
     saveButtonArray.push(saveIcon);
     inputArray.push(taskInput);
     savedSuccessfully.push(saved);
@@ -90,19 +100,24 @@ function renderElements() {
   saveTask();
 }
 
+// loop through the array to assign click event to all 'save' buttons
 function saveTask() {
   for (let index = 0; index < saveButtonArray.length; index++) {
     saveButtonArray[index].on("click", function () {
-      let taskContent = inputArray[index].val();
+      let taskContent = inputArray[index].val(); // get the content in the input field
 
+      // if the element at the index position does not exist, add the task as an array element
       if (savedTasks.task[index] === null) {
         savedTasks.task.push(taskContent);
+        // if the array element already exists, replace the string value instead of adding a new array element
       } else {
         savedTasks.task[index] = taskContent;
       }
 
       localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
       savedSuccessfully[index].css("display", "inline-block");
+
+      // when the 'save' button is clicked, a 'saved successfully' message will pop up for 1s
       const messageDisplayTimer = setInterval(function () {
         savedSuccessfully[index].css("display", "none");
         clearInterval(messageDisplayTimer);
