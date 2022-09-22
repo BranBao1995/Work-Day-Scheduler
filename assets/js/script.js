d5f607c564b7d41d5ad889b6e43754b83e64f3ba
BranBao1995 <brandon.0702@hotmail.com>
2022-09-22 16:06:01 -0400

    added some more css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,77 +17,96 @@ const timeSlots = [
   "5PM",
   "6PM",
   "7PM",
-  "8PM",
-  "9PM",
-  "10PM",
 ];
 
 let saveButtonArray = [];
 let inputArray = [];
+let savedSuccessfully = [];
 
 let savedTasks = {
   task: [],
 };
 
-if (localStorage.getItem("savedTasks") != null) {
-  savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
-} else {
-  savedTasks = {
-    timeBlock: [],
-    task: [],
-  };
-}
+init();
 
-const timeBlocksUl = $("<ul>");
-timeBlocksUl.attr("class", "custom-timeBlocks");
-timeBlocksContainer.append(timeBlocksUl);
-
-currentDayDisplay.text(currentDay);
-
-for (let i = 0; i < timeSlots.length; i++) {
-  let timeBlock = $("<li>");
-  let timeLabel = $("<label>");
-  let taskInput = $("<input>");
-  let saveIcon = $("<button>");
-
-  timeLabel.text(timeSlots[i]);
-  taskInput.val(savedTasks.task[i]);
-
-  timeBlocksUl.append(timeBlock);
-  timeBlock.append(timeLabel).append(taskInput).append(saveIcon);
-  saveIcon.append('<i class="icofont-save icofont-2x"></i>');
-
-  taskInput.attr("id", "timeBlock" + i);
-  taskInput.attr("type", "text");
-  saveIcon.attr("id", "timeBlock" + i);
-  saveIcon.children().attr("id", "timeBlock" + i);
-
-  timeBlock.addClass("custom-time-block custom-row");
-  timeLabel.addClass("custom-hour");
-  saveIcon.addClass("custom-saveBtn");
-
-  if (moment(timeSlots[i], "hA").isSame(currentTime)) {
-    taskInput.addClass("custom-input custom-present");
-  } else if (moment(timeSlots[i], "hA").isBefore(currentTime)) {
-    taskInput.addClass("custom-input custom-past");
-  } else if (moment(timeSlots[i], "hA").isAfter(currentTime)) {
-    taskInput.addClass("custom-input custom-future");
+function init() {
+  if (localStorage.getItem("savedTasks") != null) {
+    savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
+  } else {
+    savedTasks = {
+      task: [],
+    };
   }
 
-  saveButtonArray.push(saveIcon);
-  inputArray.push(taskInput);
+  currentDayDisplay.text(currentDay);
+
+  renderElements();
 }
 
-for (let index = 0; index < saveButtonArray.length; index++) {
-  saveButtonArray[index].on("click", function () {
-    let taskContent = inputArray[index].val();
+function renderElements() {
+  const timeBlocksUl = $("<ul>");
+  timeBlocksUl.attr("class", "custom-timeBlocks");
+  timeBlocksContainer.append(timeBlocksUl);
 
-    if (savedTasks.task[index] === null) {
-      savedTasks.task.push(taskContent);
-    } else {
-      savedTasks.task[index] = taskContent;
+  for (let i = 0; i < timeSlots.length; i++) {
+    let timeBlock = $("<li>");
+    let timeLabel = $("<label>");
+    let taskInput = $("<input>");
+    let saveIcon = $("<button>");
+    let saved = $("<span>");
+
+    timeLabel.text(timeSlots[i]);
+    saved.text("Saved Successfully");
+    taskInput.val(savedTasks.task[i]);
+
+    timeBlocksUl.append(timeBlock);
+    timeBlock
+      .append(timeLabel)
+      .append(taskInput)
+      .append(saved)
+      .append(saveIcon);
+    saveIcon.append('<i class="icofont-save icofont-2x"></i>');
+
+    taskInput.attr("type", "text");
+
+    timeBlock.addClass("custom-time-block custom-row");
+    timeLabel.addClass("custom-hour");
+    saved.addClass("custom-saved");
+    saveIcon.addClass("custom-saveBtn");
+
+    if (moment(timeSlots[i], "hA").isSame(currentTime)) {
+      taskInput.addClass("custom-input custom-present");
+    } else if (moment(timeSlots[i], "hA").isBefore(currentTime)) {
+      taskInput.addClass("custom-input custom-past");
+    } else if (moment(timeSlots[i], "hA").isAfter(currentTime)) {
+      taskInput.addClass("custom-input custom-future");
     }
 
-    localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
-  });
+    saveButtonArray.push(saveIcon);
+    inputArray.push(taskInput);
+    savedSuccessfully.push(saved);
+  }
+
+  saveTask();
+}
+
+function saveTask() {
+  for (let index = 0; index < saveButtonArray.length; index++) {
+    saveButtonArray[index].on("click", function () {
+      let taskContent = inputArray[index].val();
+
+      if (savedTasks.task[index] === null) {
+        savedTasks.task.push(taskContent);
+      } else {
+        savedTasks.task[index] = taskContent;
+      }
+
+      localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
+      savedSuccessfully[index].css("display", "inline-block");
+      const messageDisplayTimer = setInterval(function () {
+        savedSuccessfully[index].css("display", "none");
+        clearInterval(messageDisplayTimer);
+      }, 1000);
+    });
+  }
 }
